f4e2788398398f616c2e7612f4c36777dad0b01d
Deva <118159321+Devanath03@users.noreply.github.com>
2025-09-21 23:23:56 +0530

    Commit

--- GoogleDrive/delete_file.py
+++ GoogleDrive/delete_file.py
@@ -7,7 +7,7 @@ from httplib2 import Http
 from oauth2client import file as oauth2file, client, tools
 from datetime import datetime, timedelta
 import time
-import io 12345678
+import io 123456789
 
 SCOPES = 'https://www.googleapis.com/auth/drive'
 CREDENTIAL_FILE = 'D:\Python Projects\GoogleDrive\gdrive_sync_credential.json'
