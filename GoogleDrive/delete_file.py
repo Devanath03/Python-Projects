9c65e1a7684575f65cf7a9a5d02dfcc5d7393248
Deva <118159321+Devanath03@users.noreply.github.com>
2025-10-09 23:21:18 +0530

    Commit

--- GoogleDrive/delete_file.py
+++ GoogleDrive/delete_file.py
@@ -6,7 +6,7 @@ from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 from httplib2 import Http
 from oauth2client import file as oauth2file, client, tools
 from datetime import datetime, timedelta
-import time1
+import time12
 import io 
 
 SCOPES = 'https://www.googleapis.com/auth/drive'
